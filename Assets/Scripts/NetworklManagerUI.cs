6060a021f9a134e5325fff255869936e1aef9e4d
William-Fraser <70350291+William-Fraser@users.noreply.github.com>
2026-02-16 02:05:00 -0400

    networkUI for relay

--- Assets/Scripts/NetworklManagerUI.cs
+++ Assets/Scripts/NetworklManagerUI.cs
@@ -1,23 +1,46 @@
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class NetworklManagerUI : MonoBehaviour
 {
+    //networking
     [SerializeField] private Button serverBtn;
     [SerializeField] private Button hostBtn;
     [SerializeField] private Button clientBtn;
 
+    //relay
+    [SerializeField] private Button genJoinCodeBtn;
+    [SerializeField] private TextMeshProUGUI joinCodeTxt;
+
+    public delegate void SetCodeAsync(string code);
+    public SetCodeAsync JoinCodeAsync;
+
     private void Awake()
     {
-        serverBtn.onClick.AddListener(() => {
+        JoinCodeAsync = SetJoinCode;
+
+        serverBtn.onClick.AddListener(() => 
+        {
             NetworkManager.Singleton.StartServer();
         });
-        hostBtn.onClick.AddListener(() => {
+        hostBtn.onClick.AddListener(() => 
+        {
             NetworkManager.Singleton.StartHost();
         });
-        clientBtn.onClick.AddListener(() => {
+        clientBtn.onClick.AddListener(() => 
+        {
             NetworkManager.Singleton.StartClient();
         });
+        genJoinCodeBtn.onClick.AddListener(() =>
+        {
+            TestRelay.Singleton.CreateRelay(JoinCodeAsync);
+        });
+    }
+
+    private void SetJoinCode(string joinCode)
+    {
+        joinCodeTxt.SetText(joinCode);
     }
 }
--- Assets/Scripts/TestRelay.cs
+++ Assets/Scripts/TestRelay.cs
@@ -1,9 +1,40 @@
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 
 public class TestRelay : MonoBehaviour
 {
-    void Start()
+    public static TestRelay Singleton;
+
+    private async void Start()
     {
-        UnityServices.InitailizeAsync(); 
+        if (Singleton != null) Destroy(this);
+        Singleton = this;
+
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+            Debug.Log("Signed in" + AuthenticationService.Instance.PlayerId);
+        };
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        
+    }
+
+    public async void CreateRelay(NetworklManagerUI.SetCodeAsync SetJoinCode)
+    {
+        try
+        {
+            Allocation allocation = await RelayService.Instance.CreateAllocationAsync(3);
+
+            string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+
+            SetJoinCode(joinCode);
+        }
+        catch (RelayServiceException e) {
+            Debug.Log(e);
+        };
     }
 }
